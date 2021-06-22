58811e100306a5c1c97d6dbc972bfcf1e2955ccf
Deepak Pandey <d.pandey11585@gmail.com>
2021-06-22 09:59:09 +0530

    Initial Commit'

--- TapeEquillibrium.cpp
+++ TapeEquillibrium.cpp
@@ -5,16 +5,16 @@
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
-int solution(vector<int> &A) {
+int solution(vector <int> &A) {
     // write your code in C++14 (g++ 6.2.0)
     int totalSum = 0;
-    for(auto elem : A)
+    for (auto elem : A)
         totalSum += elem;
         
     int sum_so_far = 0;
     int minDiff = INT_MAX;
     
-    for(auto elem : A) {
+    for (auto elem : A) {
         minDiff = min(minDiff, abs(totalSum - sum_so_far));
         totalSum -= elem;
         sum_so_far += elem;
